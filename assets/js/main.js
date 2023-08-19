c683f6eedc9c310100fdb0296014eaa32ae695c3
Adrian Milano <adrian.milano@paper.id>
2023-08-19 22:18:34 +0700

    Update Favicon, And Phone Information

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+  var mastheadheight = $(".ds-header").outerHeight();
+  //console.log(mastheadheight);
+  $(".ds-banner,.ds-main-section").css("margin-top", mastheadheight);
 
-
-	var mastheadheight = $('.ds-header').outerHeight();
-	//console.log(mastheadheight);
-	$(".ds-banner,.ds-main-section").css("margin-top" , mastheadheight);
-
-	$(window).scroll(function(){
-	    if ($(window).scrollTop() >= 10) {
-	        $('.ds-header').addClass('ds-fixed-header');
-	    }
-	    else {
-	        $('.ds-header').removeClass('ds-fixed-header');
-	    }
-	}).scroll();
-
-
+  $(window)
+    .scroll(function () {
+      if ($(window).scrollTop() >= 10) {
+        $(".ds-header").addClass("ds-fixed-header");
+      } else {
+        $(".ds-header").removeClass("ds-fixed-header");
+      }
+    })
+    .scroll();
 });
+
+function copyToClipboard(phone) {
+  navigator.clipboard.writeText(phone);
+  alert("Number Phone Copied");
+}
